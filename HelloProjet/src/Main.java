f6a94c35cd7d7cc7435257787b65f69d65181a2c
jiangzhenxi <17692541413@163.com>
2019-05-31 16:50:15 +0800

    李四的提交

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("这是新增的代码!");
+        System.out.println("李四的提交!");
     }
 }
